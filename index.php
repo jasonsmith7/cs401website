20d78187261ba7a5a30117b77ad925ab89452f86
jason smith <jasonsmith7@u.boisestate.edu>
2018-09-17 17:14:08 -0600

    initialize project

---
+++ index.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+<link href="myStyle.css">
+<link rel="icon" href="http://icons.iconarchive.com/icons/icons8/ios7/512/Music-Guitar-icon.png">
+<title>PHP Test</title>
+<h1><img src="logo.jpeg"/></h1>
+</head>
+<body>
+
+
+<?php echo "<body style='background-color:#FFEEAA'>";
+	  echo '<p>Hello World</p>'; ?>
+</body>
+</html>
