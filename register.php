32d03c90473661a7a6ee6e86c67734ae193623a7
jason smith <jasonsmith7@u.boisestate.edu>
2018-09-26 19:07:06 -0600

    more changes (favicon)

--- register.php
+++ register.php
@@ -3,7 +3,7 @@
 <head>
 <link href="style_home.css">
 <link href="register_html.html">
-<link rel="icon" href="images/guitar_icon.png">
+<link rel="icon" type="image/png" href="https://png2.kisspng.com/sh/75e87d654446d3e2298e60f4dd619c15/L0KzQYm3U8IxN5R2fZH0aYP2gLBuTfNwdaF6jNd7LXnmf7B6Tfd2caVmip9tb4fxfLFohL1ncZ1qi59wdXn3ccO0hwJmbV46edQ6MEi0QoO6VsI6O182UaUDNkOzR4K8UsE2PWI4T6o6NEWxgLBu/kisspng-computer-icons-guitar-download-files-guitar-free-5ab10812236293.193863071521551378145.png">
 <title>Jay String Jewelry - Register</title>
 </head>
 <body>
--- shop.php
+++ shop.php
@@ -3,8 +3,8 @@
 <head>
 <link href="style_home.css">
 <link href="shop_html.html'>
-<link rel="icon" href="images/guitar_icon.png">
-<title>PHP Test</title>
+<link rel="icon" type="image/png" href="https://png2.kisspng.com/sh/75e87d654446d3e2298e60f4dd619c15/L0KzQYm3U8IxN5R2fZH0aYP2gLBuTfNwdaF6jNd7LXnmf7B6Tfd2caVmip9tb4fxfLFohL1ncZ1qi59wdXn3ccO0hwJmbV46edQ6MEi0QoO6VsI6O182UaUDNkOzR4K8UsE2PWI4T6o6NEWxgLBu/kisspng-computer-icons-guitar-download-files-guitar-free-5ab10812236293.193863071521551378145.png">
+<title>Jay String Jewelry - Shop</title>
 </head>
 <body>
 <?php echo "<body style='background-color:#FFEEAA'>";?>
