786a56f8bf30d95a48ce9d072576cbb5d8fdfe4c
jason smith <jasonsmith7@u.boisestate.edu>
2018-12-03 10:29:24 -0700

    contact fills email and added venmo

--- contact.php
+++ contact.php
@@ -1,3 +1,9 @@
+<?php
+//session_start();
+  $email = "";
+  if (isset($_SESSION["email_preset"])) {
+    $email = $_SESSION["email_preset"];
+  }?>
 <div class="second" id="contact">
 	<p class="second">Leave a comment or request a product</p>
 	<p class="second">Please leave a comment about Jay or his products. If there is a concern, Jay will
@@ -7,10 +13,12 @@
 	<div>
 
 	<form align="center" width="100%" action="comment_handler.php" method="POST">
-		<br><input placeholder="email address:"name="email" type="email" /><br/>
+		<br><input placeholder="email address:"name="email" type="email" value="<?php echo $email; ?>" /><br/>
 		<br><textarea class="ta" rows="5" name="comment">Leave a comment!!!</textarea></br>
 		<input type="submit" value="SEND" class="submit" />
 	</form>
+	<div>If you need to pay Jay, please do so through Venmo! Here is his info:</div>
+	<img src="images/venmo.png">
 	</div>
 	<!--<table><?php
 	#session_start();
